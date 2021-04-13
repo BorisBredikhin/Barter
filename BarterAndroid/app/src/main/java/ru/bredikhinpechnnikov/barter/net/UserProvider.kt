993df4701416c27cc2152bf95e4c182a9c7abd56
Boris Bredikhin <boris00043@yandex.ru>
2021-04-13 15:01:24 +0500

    show userpage

---
+++ BarterAndroid/app/src/main/java/ru/bredikhinpechnnikov/barter/net/UserProvider.kt
@@ -0,0 +1,35 @@
+package ru.bredikhinpechnnikov.barter.net
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import ru.bredikhinpechnnikov.barter.data.model.Profile
+
+fun getUserData(token: String): Profile {
+    var response: Profile? = null
+    var resp: String? = null
+
+    val thr = Thread(kotlinx.coroutines.Runnable {
+        OkHttpClient()
+            .newCall(
+                Request
+                    .Builder()
+                    .url(Config.BACKEND_ADDRESS + "/api/profile/")
+                    .header("Authorization", "token $token")
+                    .get()
+                    .build()
+            )
+            .execute()
+            .use {
+                resp = it.body!!.string()
+                response = Profile(resp!!)
+            }
+    })
+
+
+    thr.start()
+    thr.join()
+
+    return response!!
+}
--- BarterAndroid/app/src/main/java/ru/bredikhinpechnnikov/barter/ui/AppActivity.kt
+++ BarterAndroid/app/src/main/java/ru/bredikhinpechnnikov/barter/ui/AppActivity.kt
@@ -1,57 +1,35 @@
 package ru.bredikhinpechnnikov.barter.ui
 
-import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import ru.bredikhinpechnnikov.barter.R
+import ru.bredikhinpechnnikov.barter.data.model.Profile
+import ru.bredikhinpechnnikov.barter.net.getUserData
+import ru.bredikhinpechnnikov.barter.userToken
 
 class AppActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
+    private lateinit var userdata: Profile
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
+        Log.d("barter", intent.getStringExtra("token") ?: "")
+   }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.app, menu)
-        return true
-    }
+    @SuppressLint("SetTextI18n")
+    override fun onStart() {
+        super.onStart()
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        Log.d("barter", intent.getStringExtra("token") ?: "")
+
+        val token = intent.getStringExtra("token")
+        userdata = getUserData(token!!)
+        findViewById<TextView>(R.id.username).text = "${userdata.firstName} ${userdata.lastName}"
+
     }
 }
